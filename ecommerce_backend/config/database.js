071f0f19164b671aaacdd00c04d2b893f2c78891
piyalidas10 <piyalidas.it@gmail.com>
2019-09-29 09:41:09 +0530

    make DB secure

--- ecommerce_backend/config/database.js
+++ ecommerce_backend/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     mlab:
     {
         name: "piyali",
-        url: "mongodb://piyali:admin123@ds341557.mlab.com:41557/ecommerce",
+        url: "mongodb://<dbuser>:<dbpassword>@ds341557.mlab.com:41557/ecommerce",
         port: 27017
     }
 };
