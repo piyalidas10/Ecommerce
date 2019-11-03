afa53d3b5ae1acb74e2494653630ee1a6ab45038
piyalidas10 <piyalidas.it@gmail.com>
2019-11-03 12:16:09 +0530

    page refresh issue fix using useHash

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -57,7 +57,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
     exports: [RouterModule],
     providers: [AuthGuard, LoginGuard]
 })
